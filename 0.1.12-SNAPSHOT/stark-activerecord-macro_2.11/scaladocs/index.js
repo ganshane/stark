cc5b665cb1e012f2bdc1f2c171d782420607a016
Jun Tsai <jun.tsai@gmail.com>
2016-11-28 12:49:40 +0000

    Creating site for 0.1.12-SNAPSHOT

---
+++ 0.1.12-SNAPSHOT/stark-activerecord-macro_2.11/scaladocs/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"stark" : [], "stark.activerecord" : [], "stark.activerecord.macroinstruction" : [{"object" : "stark\/activerecord\/macroinstruction\/ActiveRecordMacroDefinition$.html", "name" : "stark.activerecord.macroinstruction.ActiveRecordMacroDefinition"}]};
