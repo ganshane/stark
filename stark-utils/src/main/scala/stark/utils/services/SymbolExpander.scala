a8082c4588fa1f13e86dc201e3b807e4063a4a5c
Jun Tsai <jcai@ganshane.com>
2017-02-24 13:45:11 +0800

    用spring实现功能

--- stark-utils/src/main/scala/stark/utils/services/SymbolExpander.scala
+++ stark-utils/src/main/scala/stark/utils/services/SymbolExpander.scala
@@ -2,13 +2,12 @@
 // site: http://www.ganshane.com
 package stark.utils.services
 
-import org.apache.tapestry5.ioc.internal.services.{MapSymbolProvider, SymbolSourceImpl, SystemEnvSymbolProvider, SystemPropertiesSymbolProvider}
-
-import scala.collection.JavaConversions._
+import org.springframework.util.{PropertyPlaceholderHelper, SystemPropertyUtils}
 
 /**
  * 提供解析表达式
- * @author jcai
+  *
+  * @author jcai
  */
 object SymbolExpander {
 
@@ -16,19 +15,34 @@ object SymbolExpander {
    * 解析字符串
    */
   def expand(input: String, params: Map[String, String] = Map[String, String]()) = {
-    val symbolSource = new SymbolSourceImpl(List(
-      new SystemPropertiesSymbolProvider,
-      new SystemEnvSymbolProvider,
-      new MapSymbolProvider(params)
-    ))
-    symbolSource.expandSymbols(input)
+    val symbolSource = new SymbolSource(input,params)
+    val helper = new PropertyPlaceholderHelper(SystemPropertyUtils.PLACEHOLDER_PREFIX,SystemPropertyUtils.PLACEHOLDER_SUFFIX)
+    helper.replacePlaceholders(input,symbolSource)
   }
 
   def valueForSymbol(symbolName: String) = {
-    val symbolSource = new SymbolSourceImpl(List(
-      new SystemPropertiesSymbolProvider,
-      new SystemEnvSymbolProvider
-    ))
-    symbolSource.valueForSymbol(symbolName)
+    val symbolSource = new SymbolSource(symbolName,Map[String,String]())
+    symbolSource.resolvePlaceholder(symbolName)
   }
+
+  private class SymbolSource(text:String,params:Map[String,String]) extends PropertyPlaceholderHelper.PlaceholderResolver {
+
+    def resolvePlaceholder(placeholderName: String): String = {
+        params.get(placeholderName) match {
+          case Some(propVal) =>
+            propVal
+          case None =>
+            sys.props.get(placeholderName) match{
+              case Some(v)=> v
+              case None =>
+                sys.env.get(placeholderName) match{
+                  case Some(v)=> v
+                  case None =>
+                    throw new RuntimeException("Could not resolve placeholder '" + placeholderName + "' in [" + this.text + "] as system property")
+                }
+            }
+        }
+    }
+  }
+
 }
